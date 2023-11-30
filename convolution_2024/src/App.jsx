d0605d520ffef4b97b6a9fda4da2e413b312a334
UTTAM <duttau326@gmail.com>
2023-12-01 00:23:15 +0530

    NAvbar

--- convolution_2024/src/App.jsx
+++ convolution_2024/src/App.jsx
@@ -1,16 +1,18 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Uttam</h1>
+      <Header />
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- convolution_2024/src/main.jsx
+++ convolution_2024/src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header/>
     <App />
-    <Footer/>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
