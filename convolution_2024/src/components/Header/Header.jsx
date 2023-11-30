12007a867eabd33f37b5b61d48a7a32ee4c20a1c
UTTAM <duttau326@gmail.com>
2023-12-01 01:39:59 +0530

    navbar_1stDec

--- convolution_2024/src/components/Header/Header.jsx
+++ convolution_2024/src/components/Header/Header.jsx
@@ -8,17 +8,37 @@ function Header() {
         <div className="flex flex-wrap justify-around bg-black text-white text-center">
           <div className="justify-content-center my-auto">
             <ul className="flex flex-wrap justify-around">
-              <a className="m-4 p-4 nav_text_li">Home</a>
-              <a className="m-4 p-4 nav_text_li">About</a>
-              <a className="m-4 p-4 nav_text_li">Events</a>
+              <a className="group  m-4 p-4 nav_text_li  transition duration-300 ">
+                Home
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
+              <a className="group  m-4 p-4 nav_text_li  transition duration-300">
+                About
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
+              <a className="group m-4 p-4 nav_text_li transition duration-300">
+                Events
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
             </ul>
           </div>
-          <div className="justify-content-center m-4 p-4 nav_text_logo">CONVOLUTION</div>
+          <div className="justify-content-center mt-3 mb-4  p-4 nav_text_logo">
+            CONVOLUTION
+          </div>
           <div className="justify-content-center my-auto">
             <ul className="flex flex-wrap justify-around">
-              <a className="m-4 p-4 nav_text_li">Timeline</a>
-              <a className="m-4 p-4 nav_text_li">Team</a>
-              <a className="m-4 p-4 nav_text_li">Contacts</a>
+              <a className="group m-4 p-4 nav_text_li transition duration-300">
+                Timeline
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
+              <a className="group m-4 p-4 nav_text_li transition duration-300">
+                Team
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
+              <a className="group m-4 p-4 nav_text_li transition duration-300">
+                Contacts
+                <span className="mt-1 mr-2 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-orange-500 "></span>
+              </a>
             </ul>
           </div>
         </div>
