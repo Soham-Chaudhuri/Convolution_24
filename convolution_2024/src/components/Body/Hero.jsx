7ac9931bda8852f31b56fa65a2ebc6657932567b
Soham-Chaudhuri <sohamchaudhuri.12.a.38@gmail.com>
2023-12-21 21:49:18 +0530

    21.12.2023

--- convolution_2024/src/components/Body/Hero.jsx
+++ convolution_2024/src/components/Body/Hero.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import './Hero.css';
-import Hero_vd from './Hero_vd';
+import React from "react";
+import "./Hero.css";
+import Hero_vd from "./Hero_vd";
+import thunder from "../assets/Thunder.png"
 function Hero() {
   return (
     <>
-    <div className="hero_bg p-4">
-      <div className="m-4 hero_heading_1_div flex flex-wrap flex-col">
-        <span className='hero_heading_1 text-shadow-md'>JUEE STUDENTS' FORUM</span>
-        <span className='hero_heading_2 text-shadow-md'>CONVOLUTION 9.0</span>
-        <span className="hero_heading_3">The Ninth Edition of Annual Technical Meet</span>
-        <div className="flex hero_btn mt-5">
-          <button className='hero_btn_1'>SignUp</button>
-          <button className='hero_btn_2'>Login</button>
+      <div className="hero_bg p-4">
+        <div className="flex">
+          <div className="m-4 hero_heading_1_div flex flex-wrap flex-col">
+            <span className="hero_heading_1 text-shadow-md">
+              JUEE STUDENTS' FORUM
+            </span>
+            <span className="hero_heading_2 text-shadow-md">
+              CONVOLUTION 9.0
+            </span>
+            <span className="hero_heading_3">
+              The Ninth Edition of Annual Technical Meet
+            </span>
+            <div className="flex hero_btn mt-5">
+              <button className="hero_btn_1">SignUp</button>
+              <button className="hero_btn_2">Login</button>
+            </div>
+          </div>
+          <div className="hero_thunder flex items-center">
+            <img src={thunder} alt="" className="hero_thunder_img" />
+          </div>
         </div>
+        <Hero_vd />
       </div>
-    <Hero_vd/>
-    </div>
     </>
-  );    
+  );
 }
 
 export default Hero;
