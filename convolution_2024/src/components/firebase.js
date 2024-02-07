01cec6fe0c707da7dbecfda9e6a42060b23e9c46
UTTAM <duttau326@gmail.com>
2024-02-07 21:26:17 +0530

    firebase update

--- convolution_2024/src/components/firebase.js
+++ convolution_2024/src/components/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoidI8s1fNwC15jFUnB111iRJbVSnCN78",
-  authDomain: "convo-test-e2385.firebaseapp.com",
-  projectId: "convo-test-e2385",
-  storageBucket: "convo-test-e2385.appspot.com",
-  messagingSenderId: "352444834954",
-  appId: "1:352444834954:web:147f108e9e6c51ab3ef973",
-  databaseURL: "https://convo-test-e2385-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyBDwZqkhELEbMHWJP-nUQx1JnEQbdvOML0",
+  authDomain: "convo1-542d5.firebaseapp.com",
+  projectId: "convo1-542d5",
+  storageBucket: "convo1-542d5.appspot.com",
+  messagingSenderId: "1069665287756",
+  appId: "1:1069665287756:web:2c2e7e49f6c72c335dbee9",
+  databaseURL: "https://convo1-542d5-default-rtdb.firebaseio.com",
 };
 
 export const app = initializeApp(firebaseConfig);
