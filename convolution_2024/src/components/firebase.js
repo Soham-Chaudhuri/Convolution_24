aea56a0b4bfcbd03a74eb1e4b7a2c0a92e0d8f11
Soham-Chaudhuri <94887725+Soham-Chaudhuri@users.noreply.github.com>
2024-03-23 11:22:22 +0530

    RIP localhost:5173

--- convolution_2024/src/components/firebase.js
+++ convolution_2024/src/components/firebase.js
@@ -1,15 +1,7 @@
 // import { initializeApp } from "firebase/app";
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCakcrIeMdyNFVdsHWRB9yrvRw7nsFz9JU",
-//   authDomain: "convolution-2024.firebaseapp.com",
-//   databaseURL: "https://convolution-2024-default-rtdb.firebaseio.com",
-//   projectId: "convolution-2024",
-//   storageBucket: "convolution-2024.appspot.com",
-//   messagingSenderId: "675220674029",
-//   appId: "1:675220674029:web:7d2b80a4f0ba1d9a2683e4",
-//   measurementId: "G-5EL6FKX5DY",
-//   databaseURL: "https://convolution-2024-default-rtdb.firebaseio.com",
+
 // };
 
 // export const app = initializeApp(firebaseConfig);
